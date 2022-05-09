c9b586fd572399106741735a735840302b925fae
ivan <ivanpdoria@gmail.com>
2022-05-09 16:12:33 +0200

    Fix user permissions

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Create folder for logs
 mkdir -p /var/log/systemd-examples/
+chown -R ${SUDO_USER:-$USER}:${SUDO_USER:-$USER} /var/log/systemd-examples
 
 # Create folder for scripts
 mkdir -p /usr/local/bin/systemd-examples/
--- src/simple.sh
+++ src/simple.sh
@@ -4,9 +4,8 @@ iter=0;
 
 while [ $iter -lt 10 ]
 do
-    result=$(systemctl status simple.service);
+    result=$(systemctl is-active simple.service);
     echo $result # &> /var/log/systemd-examples/simple-service.log
     iter=$(( $iter + 1 ));
-    echo $iter;
     sleep 1;
 done
